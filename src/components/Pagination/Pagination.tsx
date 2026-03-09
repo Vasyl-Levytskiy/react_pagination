95c07f828898935c066321cd019c992c6e930dca
Vasyl Levitskyi <leva1990vale@gmail.com>
2026-03-09 09:04:02 +0200

    add solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,7 +3,7 @@ type Props = {
 
   perPage: number;
 
-  currentPage: number;
+  currentPage?: number;
 
   onPageChange: (page: number) => void;
 };
@@ -11,7 +11,7 @@ type Props = {
 export const Pagination = ({
   total,
   perPage,
-  currentPage,
+  currentPage = 1,
   onPageChange,
 }: Props) => {
   const totalPages = Math.ceil(total / perPage);
